172a1b32f2bff00783055f7dfe9b762a9d2eeba8
oaqque <oaqque@me.com>
2020-08-26 10:29:51 +1000

    Added azure keys setup

---
+++ src/keys.py
@@ -0,0 +1,8 @@
+"""
+This file contains any public and secret keys for accessing API's 
+"""
+
+# Enter your Azure Keys here, check documentation for more
+# information on how to setup. 
+azure_key = "[Azure_Key]"
+azure_endpoint = "[Azure_Endpoint]"
